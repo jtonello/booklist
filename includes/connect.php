82499b2cc504e94a7ca505498acd30416e084d12
John S. Tonello <21206201+jtonello@users.noreply.github.com>
2017-12-17 21:48:34 -0500

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -4,6 +4,6 @@ $dbuser = "webuser";
 $pass = "YOUR-PASSWORD";
 $host = "localhost";
 $database = "booklist";
-$mysqli = new mysqli($localhost,$dbuser,$pass,$database);
+$mysqli = new mysqli($host,$dbuser,$pass,$database);
 
 ?>
